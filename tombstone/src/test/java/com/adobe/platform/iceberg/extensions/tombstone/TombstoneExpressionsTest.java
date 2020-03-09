7c06f911b3df7280bfbcb1c659c70dedfbc02f89
Gautam Kowshik <kowshik@adobe.com>
2020-03-08 23:52:12 -0700

    fixed checkstyle errors in TombstoneExpressionsTest

--- tombstone/src/test/java/com/adobe/platform/iceberg/extensions/tombstone/TombstoneExpressionsTest.java
+++ tombstone/src/test/java/com/adobe/platform/iceberg/extensions/tombstone/TombstoneExpressionsTest.java
@@ -20,45 +20,43 @@
 package com.adobe.platform.iceberg.extensions.tombstone;
 
 import com.google.common.collect.Lists;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import org.apache.iceberg.expressions.Expression;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TombstoneExpressionsTest {
 
-    @Test
-    public void notIn() {
-        List<ExtendedEntry> tbs = Lists.newArrayList(
-                new ExtendedEntry.Builder().withEmptyProperties(() -> "a"),
-                new ExtendedEntry.Builder().withEmptyProperties(() -> "b"),
-                new ExtendedEntry.Builder().withEmptyProperties(() -> "c"),
-                new ExtendedEntry.Builder().withEmptyProperties(() -> "d")
-        );
+  @Test
+  public void notIn() {
+    List<ExtendedEntry> tbs = Lists.newArrayList(
+        new ExtendedEntry.Builder().withEmptyProperties(() -> "a"),
+        new ExtendedEntry.Builder().withEmptyProperties(() -> "b"),
+        new ExtendedEntry.Builder().withEmptyProperties(() -> "c"),
+        new ExtendedEntry.Builder().withEmptyProperties(() -> "d")
+    );
 
-        Optional<Expression> optExp = TombstoneExpressions.notIn("fld", tbs);
-        Assert.assertTrue(optExp.isPresent());
-        Expression exp = optExp.get();
-        Assert.assertEquals("(((ref(name=\"fld\") != \"a\" and ref(name=\"fld\") != \"b\") and ref(name=\"fld\") != \"c\") and ref(name=\"fld\") != \"d\")",
-                exp.toString());
+    Optional<Expression> optExp = TombstoneExpressions.notIn("fld", tbs);
+    Assert.assertTrue(optExp.isPresent());
+    Expression exp = optExp.get();
+    Assert.assertEquals("(((ref(name=\"fld\") != \"a\" and ref(name=\"fld\") != \"b\") " +
+        "and ref(name=\"fld\") != \"c\") and ref(name=\"fld\") != \"d\")", exp.toString());
 
-        Optional<Expression> optExp2 = TombstoneExpressions.notIn("fld", new ArrayList<ExtendedEntry>());
-        Assert.assertFalse(optExp2.isPresent());
-    }
+    Optional<Expression> optExp2 = TombstoneExpressions.notIn("fld", new ArrayList<ExtendedEntry>());
+    Assert.assertFalse(optExp2.isPresent());
+  }
 
-    @Test
-    public void matchesAny() {
-        Optional<Expression> optExp = TombstoneExpressions.matchesAny("fld2", Lists.newArrayList("m", "n", "o", "p"));
-        Assert.assertTrue(optExp.isPresent());
-        Expression exp = optExp.get();
-        Assert.assertEquals("(((ref(name=\"fld2\") == \"m\" or ref(name=\"fld2\") == \"n\") or ref(name=\"fld2\") == \"o\") or ref(name=\"fld2\") == \"p\")",
-                exp.toString());
-
-        Optional<Expression> optExp2 = TombstoneExpressions.matchesAny("fld", new ArrayList<String>());
-        Assert.assertFalse(optExp2.isPresent());
-    }
+  @Test
+  public void matchesAny() {
+    Optional<Expression> optExp = TombstoneExpressions.matchesAny("fld2",
+        Lists.newArrayList("m", "n", "o", "p"));
+    Assert.assertTrue(optExp.isPresent());
+    Expression exp = optExp.get();
+    Assert.assertEquals("(((ref(name=\"fld2\") == \"m\" or ref(name=\"fld2\") == \"n\") " +
+        "or ref(name=\"fld2\") == \"o\") or ref(name=\"fld2\") == \"p\")", exp.toString());
+    Optional<Expression> optExp2 = TombstoneExpressions.matchesAny("fld", new ArrayList<String>());
+    Assert.assertFalse(optExp2.isPresent());
+  }
 }
